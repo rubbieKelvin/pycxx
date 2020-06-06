64b0cdf0a2f04dc8c6ea97c4290e169960bb6a71
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2020-06-06 12:51:39 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md") as file:
 
 setuptools.setup(
     name="pycxx",
-    version="0.0.1",
+    version="0.0.2",
     author="Rubbie Kelvin Voltsman",
     author_email="rubbiekelvinvoltsman@gmail.com",
     description="simple crypting library ;)",
     long_description=descr,
     long_description_content_type="text/markdown",
     url="https://rubbiekelvin@bitbucket.org/rubbiekelvin/cxx",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0'
+    python_requires='>=3.0',
+    license="MIT"
 )
